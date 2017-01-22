9eeba61ef4fa9902710a833420aa8b8c9b3daf99
Farjad Adamjee <fadamjee@vailsys.com>
2017-01-22 03:54:16 -0600

    refactored speech2text worker

--- workers/speech_to_text_worker.rb
+++ workers/speech_to_text_worker.rb
@@ -4,38 +4,43 @@ require_relative '../services/bluemix_service'
 
 class SpeechToTextWorker
   include SuckerPunch::Job
+  workers 4
 
   def perform(api, audio_url, callback_url)
-    file_service = FileService.new(url: audio_url)
-    content = get_content(api, file_service.call)
+    begin
+      file_service = FileService.new(url: audio_url)
+      content = get_content(api, file_service.call)
+      file_service.delete
+    rescue
+      content = '{"status":"invalid file input"}'
+    end
+
     CallbackService.new(callback_url: callback_url, content: content).call
-    file_service.delete
   end
 
   private
 
-  def get_api(api)
-    case api
-      when :bluemix
-        file = 'bluemix.json'
-        klass = BluemixService
-      when :google
-        # do nothing
-      when :azure
-        # do nothing
-      else
-        # do nothing
-    end
+  def get_api(svc)
+    services = {
+        bluemix: BluemixService
+    }
+    creds = {
+        bluemix: 'bluemix.json'
+    }
 
-    api = JSON.parse(File.read(file))
+    raise unless services.has_key?(svc)
+
+    api = JSON.parse(File.read(creds[svc]))
     uri = URI(api['url'])
     api_uri = 'https://' + api['username'] + ':' + api['password'] + '@' + uri.host + uri.path
-    klass.new(api: api_uri)
+    services[svc].new(api: api_uri)
   end
 
   def get_content(api, file)
     api_service = get_api(api)
     api_service.file = file
     api_service.call
+  rescue
+    '{"status":"an error has occured"}'
   end
 end
